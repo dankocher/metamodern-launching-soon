1d982c541b6fde0715d55e7b52052b97762bf1c3
karachunvladislav <karachunvladislav@gmail.com>
2021-04-28 18:40:53 +0300

    feat: added mobile adopive

---
+++ src/assets/icons/metemodern-big-icon.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const metemodernBigIcon = (
+    <svg
+        width="224"
+        height="224"
+        viewBox="0 0 224 224"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M123.84 126.314L147.058 150.508L222.196 150.508L184.627 34.4619L123.84 126.314ZM123.84 126.314L41.8765 70.1097L4.30774 186.156H79.4453L123.84 126.314Z"
+            fill="#2C2F36"
+        />
+    </svg>
+);
+
+export default metemodernBigIcon;
--- src/assets/icons/metemodern-icon.js
+++ src/assets/icons/metemodern-icon.js
@@ -9,8 +9,8 @@ const metemodernIcon = (
         xmlns="http://www.w3.org/2000/svg"
     >
         <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M28.7485 29.3227L34.1385 34.9393L51.5812 34.9393L42.8598 8L28.7485 29.3227ZM28.7485 29.3227L9.72132 16.2754L1 43.2148H18.4426L28.7485 29.3227Z"
             fill="#2C2F36"
         />
