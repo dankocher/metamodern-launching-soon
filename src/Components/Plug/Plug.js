93431fe7be395e13df714147ab9390b64af9c0c4
Vlad <vlad@MacBook-Air-Meta.local>
2021-08-25 19:06:18 +0300

    fix: fixed contacts data

--- src/Components/Plug/Plug.js
+++ src/Components/Plug/Plug.js
@@ -4,38 +4,46 @@ import styles from "./plug.module.scss";
 import cat from "../../assets/images/problemcat.png";
 
 function Plug() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.container__topSpaser} />
-            <div className={styles.content}>
-                <div className={styles.content__description}>
-                    <span className={styles.content__description__info}>
-                        We will launch soon
-                    </span>
+  return (
+    <div className={styles.container}>
+      <div className={styles.container__topSpaser} />
+      <div className={styles.content}>
+        <div className={styles.content__description}>
+          <span className={styles.content__description__info}>
+            We will launch soon
+          </span>
 
-                    <div
-                        className={`${styles.content__description__contacts} ${styles.descriptionFont}`}
-                    >
-                        <span>Write to us</span>
-                        <a href="mailto:metamodernsales@gmail.com">
-                            metamodernsales@gmail.com
-                        </a>
-                    </div>
-                    <div
-                        className={`${styles.content__description__works} ${styles.descriptionFont}`}
-                    >
-                        <span>Our works: </span>
-                        <a href="https://dribbble.com/metamodern">Dribbble</a>
-                    </div>
-                </div>
+          <div
+            className={`${styles.content__description__mail} ${styles.descriptionFont}`}
+          >
+            <span>Write to us</span>
+            <a href="mailto:metamodernsales@gmail.com">
+              metamodern.dev@gmail.com
+            </a>
+          </div>
 
-                <div className={styles.content__imgContainer}>
-                    <img src={cat} alt={"cat"} />
-                </div>
-            </div>
-            <div className={styles.container__bottomSpaser} />
+          <div
+            className={`${styles.content__description__phone} ${styles.descriptionFont}`}
+          >
+            <span>Call us</span>
+            <a href="tel:+375295704513">+375 (29) 570-45-13</a>
+          </div>
+
+          <div
+            className={`${styles.content__description__works} ${styles.descriptionFont}`}
+          >
+            <span>Our works: </span>
+            <a href="https://dribbble.com/metamodern">Dribbble</a>
+          </div>
         </div>
-    );
+
+        <div className={styles.content__imgContainer}>
+          <img src={cat} alt={"cat"} />
+        </div>
+      </div>
+      <div className={styles.container__bottomSpaser} />
+    </div>
+  );
 }
 
 export default Plug;
